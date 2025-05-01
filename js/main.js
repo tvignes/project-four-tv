1b4a22030a5f8f1cebdc3810ee82f73650175919
tvignes <tcvignes1@gmail.com>
2025-05-01 10:32:00 -0500

    in class commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 // JS scripts placed here
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.getElementById('offcanvas-menu');
+
+menuToggle.addEventListener('click', () => {
+  const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+  menuToggle.setAttribute('aria-expanded',!expanded);
+  menu.classList.toggle('expanded');
+});
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
